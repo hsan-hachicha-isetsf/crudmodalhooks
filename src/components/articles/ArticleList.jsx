c0b85c51b9f7cf26a9d7ee4bbe07dca0aa147ac2
Hachicha Hassan <hsan.hachicha@gmail.com>
2023-08-02 15:57:57 +0200

    Commit new version

--- src/components/articles/ArticleList.jsx
+++ src/components/articles/ArticleList.jsx
@@ -1,30 +1,101 @@
-import React from 'react'
-import Table from 'react-bootstrap/Table';
-const ArticleList = ({products}) => {
+import React,{ useMemo } from 'react'
+import { MaterialReactTable } from 'material-react-table';
+import { Box } from '@mui/material';
+import Button from 'react-bootstrap/Button';
+const ArticleList = ({products,deleteProduct,a}) => {
+
+  const delpr=(productid,ref)=>{
+    deleteProduct(productid,ref)
+  }
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: 'reference', //access nested data with dot notation
+        header: 'Référence',
+        size: 100,
+      },
+      {
+        accessorKey: 'designation',
+        header: 'Désignation',
+        size: 150,
+      },
+      {
+        accessorKey: 'marque', //normal accessorKey
+        header: 'Marque',
+        size: 80,
+      },
+      {
+        accessorKey: 'prix',
+        header: 'Prix',
+        size: 80,
+      },
+      {
+        accessorKey: 'qtestock',
+        header: 'Quantité',
+        size: 80,
+      },
+      {
+        accessorKey: 'imageart',
+        header: 'Image',
+        size: 80,
+        Cell: ({ cell}) => (
+          <Box
+          sx={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: '1rem',
+          }}
+          >
+          <img
+          alt=""
+          height={100}
+          src={cell.getValue()}
+          loading="lazy"
+          style={{ borderRadius: '20%' }}
+          />
+          </Box>),
+      },
+
+      {
+        accessorKey: '_id',
+        header: 'actions',
+        size: 100,
+        Cell: ({ cell, row }) => (
+        <div >
+        <Button
+        onClick={() => {
+        console.log("modification ...")
+        }}
+        variant="warning"
+        size="md"
+        className="text-warning btn-link edit"
+        >
+        <i class="fa-solid fa-pen-to-square"></i>
+        </Button>
+        <Button
+        onClick={(e) => {
+        delpr(cell.row.original._id,cell.row.original.reference, e);
+        }}
+        variant="danger"
+        size="md"
+        className="text-danger btn-link delete"
+        >
+        <i className="fa fa-trash" />
+        </Button>
+        </div>
+        ),
+        },
+    ],
+    [products],
+  );
   return (
-    <div>
-         <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>Image</th>
-          <th>Référence</th>
-          <th>Désignation</th>
-          <th>Prix</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody >
-        {products.map((pr,index)=>
-        <tr>
-          <td><img src={pr.imageart} width={80} height={80}/></td>
-          <td>{pr.reference}</td>
-          <td>{pr.designation}</td>
-          <td>{pr.prix}</td>
-        </tr>
-        )}
-      </tbody>
-    </Table>
-    </div>
+    <>
+    {products&&
+  <div>
+       <MaterialReactTable columns={columns} data={products} />;  
+       </div>
+  }
+  </>
   )
 }
 
--- src/components/articles/ArticlesApp.jsx
+++ src/components/articles/ArticlesApp.jsx
@@ -1,8 +1,11 @@
 import React,{useState,useEffect} from 'react'
-import { fetchArticles } from '../../services/ArticleService'
+import { fetchArticles,deleteArticle } from '../../services/ArticleService'
 import ArticleList from './ArticleList'
+import { confirmAlert } from 'react-confirm-alert';
 const ArticlesApp = () => {
-    const [products,setProducts]=useState([])
+  const [a,seta]=useState(5)
+    const [products,setProducts]=useState(null)
+   
 useEffect(() => {
     listproduits()
 }, [])
@@ -11,9 +14,31 @@ fetchArticles()
 .then(res=>setProducts(res.data))
 .catch(err=>console.log(err))
 }
+
+const deleteProduct = (productId,ref) => {
+  confirmAlert({
+    title: "Confirm delete...",
+    message: " supprimer l' article: " + ref,
+    buttons: [
+    {
+    label: 'Oui',
+    onClick: () => deleteArticle(productId)
+    .then(res=>
+    setProducts(products.filter((product) => product._id !== productId)))
+    //.then(console.log("suppression effectuée avec success"))
+    .catch(error=>console.log(error))
+    },
+    {
+    label: 'Non',
+    }
+    ]
+    });
+  };
   return (
     <div>
-        <ArticleList products={products}/>
+      
+      
+        <ArticleList products={products} deleteProduct={deleteProduct} a={a}/>
     </div>
   )
 }
